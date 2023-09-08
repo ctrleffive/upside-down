59e40be8787d44c36fe90619deb3d55d5e2fa551
Chandu J S <chandujs@live.com>
2023-09-08 17:36:29 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export const App = () => {
-  return <div className="logo-mask bg-no-repeat bg-cover w-screen h-screen bg-[url('./assets/intro-bg.jpg')]" />;
+  return <div className="logo-mask bg-no-repeat bg-cover w-screen h-screen bg-[url('assets/intro-bg.jpg')]" />;
 }
